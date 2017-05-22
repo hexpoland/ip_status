95bd2512008c2d27102e8f9cab91014f34ea538a
hexpoland <wolaqu@poczta.onet.pl>
2017-05-22 22:01:50 +0200

    init

---
+++ spec/ip-status-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IpStatusView from '../lib/ip-status-view';
+
+describe('IpStatusView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
